14ca091ffbcddd80bdd7826b2379eb4d307f327d
Maram <maram.alsoudani@gmail.com>
2024-08-31 16:38:08 +0300

    Finish design and logic handling of sign up screen.

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -11,7 +11,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Timer(const Duration(seconds: 4), () {
-      Navigator.pushNamed(context, Routes.datePickerRoute);
+      Navigator.pushNamed(context, Routes.signUpScreenRoute);
     });
     return Scaffold(
       body: Image.asset(
